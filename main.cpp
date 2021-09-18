9b3fe58772d77e4bd1546e92cf9321879b294321
sustainablelab <sustainablelab@gmail.com>
2021-09-18 11:02:19 -0400

    Build from scratch using IMGUI: fail

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+#include <stdio.h>
+/* See original imgui/examples/example_glfw_opengl3/Makefile:
+ *      ocornut defines this macro:
+ *          -DIMGUI_IMPL_OPENGL_LOADER_GL3W
+ *      which means use OpenGL loader gl3w.
+ * Therefore, my OpenGL definitions are in gl3w.h.
+ *  */
+#include <GL/gl3w.h>            // Initialize with gl3wInit()
+#include <GLFW/glfw3.h>
+
+static void glfw_error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Glfw Error %d: %s\n", error, description);
+}
+
+int main(int, char**)
+{
+    // Setup window
+    glfwSetErrorCallback(glfw_error_callback);
+    if (!glfwInit())
+        return 1;
+
+    const char* glsl_version = "#version 130";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
+
+    // Create window with graphics context
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "Dear ImGui GLFW+OpenGL3 example", NULL, NULL);
+    if (window == NULL)
+        return 1;
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1); // Enable vsync
+
+    // Initialize OpenGL loader
+    bool err = gl3wInit() != 0;
+
+    if (err)
+    {
+        fprintf(stderr, "Failed to initialize OpenGL loader!\n");
+        return 1;
+    }
+
+}
